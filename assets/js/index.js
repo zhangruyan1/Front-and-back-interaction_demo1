74f02abf197a7899bfdd820fccff8d056b7a1418
张茹艳 <zry15135221543@qq.com>
2022-04-27 21:20:13 +0800

    index页面初步完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,14 +6,12 @@ $(function() {
 
   // 点击按钮，实现退出功能
   $('#btnLogout').on('click', function() {
-    // 提示用户是否确认退出
-    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
-      //do something
+    // 用户是否确认退出
+    layer.confirm('确定退出登录?', { icon: 3, title: '确认退出提示' }, function(index) {
       // 1. 清空本地存储中的 token
       localStorage.removeItem('token')
       // 2. 重新跳转到登录页面
       location.href = '/login.html'
-
       // 关闭 confirm 询问框
       layer.close(index)
     })
@@ -55,17 +53,13 @@ function renderAvatar(user) {
   $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
   // 3. 按需渲染用户的头像
   if (user.user_pic !== null) {
-    // 3.1 渲染图片头像
-    $('.layui-nav-img')
-      .attr('src', user.user_pic)
-      .show()
+    // 渲染图片头像
+    $('.layui-nav-img').attr('src', user.user_pic).show()
     $('.text-avatar').hide()
   } else {
-    // 3.2 渲染文本头像
+    // 没头像渲染文本头像
     $('.layui-nav-img').hide()
     var first = name[0].toUpperCase()
-    $('.text-avatar')
-      .html(first)
-      .show()
+    $('.text-avatar').html(first).show()
   }
 }
